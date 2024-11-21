371227b0f6b04718db8cb09b1357a2f4b8d6183e
valentinohrynt <hariyantovalentino@gmail.com>
2024-11-21 11:20:47 +0700

    Implemented password recovery system using OTP sent by email.

--- routes/routes.js
+++ routes/routes.js
@@ -7,19 +7,14 @@ const authenticateToken = require('../middleware/authenticateToken');
 
 const router = express.Router();
 
-
-// CRUD operations for Users
-// router.post('/users', userController.createUser);       // Create
-// router.get('/users', userController.getAllUsers);       // Read all
-// router.get('/users', userController.getUserById);  // Read specific
-// router.delete('/users/:id', userController.deleteUser); // Delete
-
 // Update profile
 router.put('/users', authenticateToken,userController.updateUser);
 
 // Auth Routes
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
+router.post('/request-reset', authController.requestPasswordReset);
+router.post('/reset-password', authController.resetPassword);
 
 // Note Routes
 router.get('/notes', authenticateToken, noteController.getAllNotes);
